c07301098853de540426856acb4d6cbf9bfb1f4b
Brandon Teague <teaguebrandon@outlook.com>
2025-05-05 11:39:11 -0400

    refactor: changed NoPage styling and added back link

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 const NoPage: FC = () => {
   return (
-    <>
-      <h1>No Page</h1>
-    </>
+    <section className="flex flex-col items-center p-10 text-center">
+      <h1 className="subheading">Sorry, this page doesnt exist</h1>
+      <Link
+        to="/"
+        className="bg-[var(--color-accent)] text-[white] rounded-2xl px-4 py-2 mt-4"
+      >
+        &larr; Go back to the homepage
+      </Link>
+    </section>
   );
 };
 
